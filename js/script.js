bde3a2357f2fe1f9339a19c2647df41b6a7caf3f
mlietti1 <mcf.lietti@outlook.com>
2022-10-14 09:54:03 +0200

    correzione js

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@ const limit = 100;
 for (let i = 1; i <= limit; i++){
     const box = document.createElement('div');
     box.className = 'box';
-    box.innerHTML = i;
- 
-    if(!(i % 3)){
-        box.classList.add('yellow');
-        box.innerHTML = 'Fizz';
-    }if(!(i % 5)){
+    
+    if(!(i % 15)){
+        box.classList.add('red');
+        box.innerHTML = 'FizzBuzz';   
+    }else if(!(i % 5)){
         box.classList.add('orange');
         box.innerHTML = 'Buzz';
-    }if(!(i % 3 || i % 5)){
-        box.classList.add('red');
-        box.innerHTML = 'FizzBuzz';
+    }else if(!(i % 3)){
+        box.classList.add('yellow');
+        box.innerHTML = 'Fizz';
+    }else{
+        box.innerHTML = i;
     }
     
     container.append(box);
